c097694045a753d2120a40d88d524ff433986fa2
Anders <anders@somewhere.com>
2022-12-24 14:32:52 -0700

    Test message source class

---
+++ src/message_source_test.js
@@ -0,0 +1,30 @@
+function MessageSource() {
+
+    this.listeners = [];
+    this.messageInterval = null;
+
+    this.addListener = (handler) => {
+        this.listeners.push(handler);
+    }
+
+    this.close = () => {
+        this.listeners = [];
+        clearInterval(this.messageInterval); 
+        console.log("CLOSED");
+    }
+
+     this.messageInterval = setInterval(() => {
+        this.listeners.forEach((handler) => {
+            let message = "";
+            let length = Math.floor(Math.random() * 100);
+            for (let i = 0; i <= length; i++ ) {
+                let charCode = Math.floor(Math.random() * 25) + 65;
+                message += String.fromCharCode(charCode);
+            }
+            handler(message);
+        })
+    }, 100);
+
+}
+
+export { MessageSource }
